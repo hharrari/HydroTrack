879023107004aa940fac8abf83bc2d648196c731
Hiba Harrari <hharrari@css.edu>
2025-12-19 23:02:48 +0000

    Try fixing this error: `Console Error: Firebase config value "apiKey" is

--- src/config/firebase.ts
+++ src/config/firebase.ts
@@ -10,18 +10,18 @@
 // NEXT_PUBLIC_FIREBASE_APP_ID=...
 
 const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  apiKey: "YOUR_API_KEY",
+  authDomain: "YOUR_AUTH_DOMAIN",
+  projectId: "YOUR_PROJECT_ID",
+  storageBucket: "YOUR_STORAGE_BUCKET",
+  messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
+  appId: "YOUR_APP_ID",
 };
 
 // Check that all environment variables are set
 for (const [key, value] of Object.entries(firebaseConfig)) {
-    if (!value) {
-        console.error(`Firebase config value "${key}" is not set. Please check your .env.local file.`);
+    if (!value || value.startsWith('YOUR_')) {
+        console.error(`Firebase config value "${key}" is not set. Please check your firebase config.`);
     }
 }
 
