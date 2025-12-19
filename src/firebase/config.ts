c5a8df03384f620dc7352b8b4f93a426b5afc450
Hiba Harrari <hharrari@css.edu>
2025-12-19 23:23:20 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-3993683728-fa9f1",
+  "appId": "1:939205078514:web:71abd8bf5f8f1f30ad0f6b",
+  "apiKey": "AIzaSyCwGVZLp0Tu_CslSd2cLX2uErM-4ZZAda8",
+  "authDomain": "studio-3993683728-fa9f1.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "939205078514"
+};
